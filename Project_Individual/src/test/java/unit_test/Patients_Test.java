4937b672b8b54b135f9792bc4eee965444203920
Anas Ahmad,Anas A <a.anasahmad@student.fontys.nl>
2020-09-17 16:45:37 +0200

    UnitTest Testing

---
+++ Project_Individual/src/test/java/unit_test/Patients_Test.java
@@ -0,0 +1,13 @@
+package unit_test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Patients_Test {
+    @Test
+    public void FirstTest(){
+
+        assertEquals(1,1);
+    }
+}
